3e17ab6b37e691d970c17aa391fceab38054ad6e
Arnold Chand <creativenull@outlook.com>
2021-08-02 12:31:53 -0400

    feat(config): limit config options

--- denops/projectlocal/config.ts
+++ denops/projectlocal/config.ts
@@ -1,64 +1,55 @@
 import { Denops, nvimFn, has, expand, getcwd } from "./deps/denops_std.ts";
 
 export interface UserConfig {
-  allowlistName: string;
-  cacheDirectory: string;
   showMessage: boolean;
-  projectConfig: {
-    filepath: string;
-  };
+  projectConfig: string;
 }
 
 export interface PartialUserConfig {
-  allowlistName?: string;
-  cacheDirectory?: string;
   showMessage?: boolean;
-  projectConfig?: {
-    filepath?: string;
-  };
+  projectConfig?: string;
 }
 
 const defaultConfig: UserConfig = {
-  allowlistName: "allowlist",
-  cacheDirectory: "",
   showMessage: true,
-  projectConfig: {
-    filepath: ".vim/init.vim",
-  },
+  projectConfig: ".vim/init.vim",
 };
 
+async function getDefaultCacheDirectory(denops: Denops): Promise<string> {
+  let cachepath: unknown;
+  if (await has(denops, "nvim")) {
+    cachepath = await nvimFn.stdpath(denops, "cache");
+    return `${cachepath}/projectlocal`;
+  } else {
+    if (Deno.build.os === "darwin") {
+      cachepath = "$HOME/Library/Caches/vim/projectlocal";
+      return await expand(denops, cachepath) as string;
+    } else if (Deno.build.os === "windows") {
+      cachepath = "$HOME/AppData/Temp/vim/projectlocal";
+      return await expand(denops, cachepath) as string;
+    } else {
+      cachepath = "$HOME/.cache/vim/projectlocal";
+      return await expand(denops, cachepath) as string;
+    }
+  }
+}
+
 export class Config {
+  private allowlistName = 'allowlist';
+  private cacheDirectory = '';
+
   constructor(private denops: Denops, private config: UserConfig) {}
 
-  static async getDefaultCacheDirectory(denops: Denops): Promise<string> {
-    let cachepath: unknown;
-    if (await has(denops, "nvim")) {
-      cachepath = await nvimFn.stdpath(denops, "cache");
-      return `${cachepath}/projectlocal`;
-    } else {
-      // TODO: Find out how to remove type conversion from string to unknown
-      if (Deno.build.os === "linux") {
-        cachepath = "$HOME/.cache/vim/projectlocal";
-        // @ts-expect-error: Unknown type conversion
-        return await expand(denops, cachepath);
-      } else if (Deno.build.os === "windows") {
-        cachepath = "$HOME/AppData/Temp/vim/projectlocal";
-        // @ts-expect-error: Unknown type conversion
-        return await expand(denops, cachepath);
-      } else {
-        cachepath = "$HOME/Library/Caches/vim/projectlocal";
-        // @ts-expect-error: Unknown type conversion
-        return await expand(denops, cachepath);
-      }
-    }
+  setCacheDirectory(value: string): void {
+    this.cacheDirectory = value;
   }
 
   getCacheDirectory(): string {
-    return this.config.cacheDirectory;
+    return this.cacheDirectory;
   }
 
   getAllowlistPath(): string {
-    return `${this.config.cacheDirectory}/${this.config.allowlistName}.json`;
+    return `${this.cacheDirectory}/${this.allowlistName}.json`;
   }
 
   async getProjectRoot(): Promise<string> {
@@ -67,15 +58,19 @@ export class Config {
 
   async getProjectConfigFilepath(): Promise<string> {
     const projectRoot = await this.getProjectRoot();
-    return `${projectRoot}/${this.config.projectConfig.filepath}`;
+    return `${projectRoot}/${this.config.projectConfig}`;
   }
 
   isProjectConfigLua(): boolean {
-    return this.config.projectConfig.filepath.endsWith(".lua");
+    return this.config.projectConfig.endsWith(".lua");
   }
 
   isProjectConfigVim(): boolean {
-    return this.config.projectConfig.filepath.endsWith(".vim");
+    return this.config.projectConfig.endsWith(".vim");
+  }
+
+  canSendMessage(): boolean {
+    return this.config.showMessage;
   }
 }
 
@@ -83,8 +78,11 @@ export class Config {
 export async function makeConfig(denops: Denops, config: PartialUserConfig): Promise<Config> {
   const userConfig: UserConfig = {
     ...defaultConfig,
-    cacheDirectory: config.cacheDirectory ?? (await Config.getDefaultCacheDirectory(denops)),
+    ...config as UserConfig,
   };
 
-  return new Config(denops, userConfig);
+  const pluginConfig = new Config(denops, userConfig);
+  pluginConfig.setCacheDirectory(await getDefaultCacheDirectory(denops) as string);
+
+  return pluginConfig;
 }
