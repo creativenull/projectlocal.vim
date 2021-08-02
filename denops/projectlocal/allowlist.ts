5f2833b2c68da8214b1c011b88f406ac1e61a3e5
Arnold Chand <creativenull@outlook.com>
2021-08-02 12:58:41 -0400

    feat(allowlist): move same to allowlist module

--- denops/projectlocal/allowlist.ts
+++ denops/projectlocal/allowlist.ts
@@ -1,14 +1,31 @@
 import { Config } from "./config.ts";
 import { hashFileContents } from "./hasher.ts";
 
-export interface AllowedProject {
+/* Allowlist sample structure:
+[
+  {
+    "projectDirectoryPath": "/path/to/projectdir",
+    "configFileHash": "89yu80yh9dy890gbfaewasdf89yb",
+    "autoload": true,
+    "ignore": false
+  },
+  {
+    "projectDirectoryPath": "/path/to/projectdir2",
+    "configFileHash": "89yu80yh9dy890gbfaewasdf89yb",
+    "autoload": true,
+    "ignore": false
+  }
+]
+*/
+
+export interface AllowlistItem {
   projectDirectoryPath: string;
   configFileHash: string;
   autoload: boolean;
   ignore: boolean;
 }
 
-export interface PartialAllowedProject {
+export interface PartialAllowlistItem {
   projectDirectoryPath?: string;
   configFileHash?: string;
   autoload?: boolean;
@@ -24,11 +41,11 @@ export interface PartialAllowedProject {
  */
 export async function addProjectConfigFile(config: Config, ignore?: boolean): Promise<void> {
   const fileContents = Deno.readTextFileSync(config.getAllowlistPath());
-  let json: AllowedProject[];
+  let json: AllowlistItem[];
   if (fileContents === "") {
     json = [];
   } else {
-    json = JSON.parse(fileContents) as AllowedProject[];
+    json = JSON.parse(fileContents) as AllowlistItem[];
   }
 
   json.push({
@@ -53,7 +70,7 @@ export function removeProjectConfigFile(config: Config): void {
     return;
   }
 
-  const json = JSON.parse(fileContents) as AllowedProject[];
+  const json = JSON.parse(fileContents) as AllowlistItem[];
   const newJsonContents = json.filter(async (item) => item.projectDirectoryPath !== (await config.getProjectRoot()));
   Deno.writeTextFileSync(config.getAllowlistPath(), JSON.stringify(newJsonContents));
 }
@@ -62,16 +79,16 @@ export function removeProjectConfigFile(config: Config): void {
  * Update a project dirpath settings from the allowlist
  *
  * @param {Config} config
- * @param {PartialAllowedProject} newSettings
+ * @param {PartialAllowlistItem} newSettings
  * @returns {void}
  */
-export async function updateProjectConfigFile(config: Config, newSettings: PartialAllowedProject): Promise<void> {
+export async function updateProjectConfigFile(config: Config, newSettings: PartialAllowlistItem): Promise<void> {
   const fileContents = Deno.readTextFileSync(config.getAllowlistPath());
   if (fileContents === "") {
     return;
   }
 
-  const json = JSON.parse(fileContents) as AllowedProject[];
+  const json = JSON.parse(fileContents) as AllowlistItem[];
   const projectRoot = await config.getProjectRoot();
   const ind = json.findIndex((item) => item.projectDirectoryPath === projectRoot);
 
@@ -79,17 +96,6 @@ export async function updateProjectConfigFile(config: Config, newSettings: Parti
     ...json[ind],
     ...newSettings,
   };
-
-  /* const newJsonContents = json.map(async (item) => {
-    if (item.projectDirectoryPath === (await config.getProjectRoot())) {
-      return {
-        ...item,
-        ...newSettings,
-      };
-    }
-
-    return item;
-  }); */
 
   Deno.writeTextFileSync(config.getAllowlistPath(), JSON.stringify(json));
 }
@@ -101,13 +107,13 @@ export async function updateProjectConfigFile(config: Config, newSettings: Parti
  * @param {string} projectpath
  * @returns {void}
  */
-export function getProjectConfig(config: Config, projectpath: string): AllowedProject | null {
+export function getProjectConfig(config: Config, projectpath: string): AllowlistItem | null {
   const fileContents = Deno.readTextFileSync(config.getAllowlistPath());
   if (fileContents === "") {
     return null;
   }
 
-  const json = JSON.parse(fileContents) as AllowedProject[];
+  const json = JSON.parse(fileContents) as AllowlistItem[];
   const [project] = json.filter((item) => item.projectDirectoryPath === projectpath);
   return project;
 }
@@ -124,7 +130,7 @@ export function isAllowed(config: Config): boolean {
     return false;
   }
 
-  const json = JSON.parse(fileContents) as AllowedProject[];
+  const json = JSON.parse(fileContents) as AllowlistItem[];
   const res = json.filter(
     async (item) => item.projectDirectoryPath === (await config.getProjectRoot()) && !item.ignore,
   );
