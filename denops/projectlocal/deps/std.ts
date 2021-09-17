e381b09260b73d3e49d08bda970afdbddd7853e3
Arnold Chand <creativenull@outlook.com>
2021-09-17 18:36:22 -0400

    feat(project): bump denops, deno_std, unknownutil

--- denops/projectlocal/deps/std.ts
+++ denops/projectlocal/deps/std.ts
@@ -1,3 +1,3 @@
-export * as hash from "https://deno.land/std@0.102.0/hash/mod.ts"
-export * as fs from "https://deno.land/std@0.102.0/fs/mod.ts"
-export * as log from "https://deno.land/std@0.102.0/log/mod.ts"
+export * as hash from "https://deno.land/std@0.107.0/hash/mod.ts"
+export * as fs from "https://deno.land/std@0.107.0/fs/mod.ts"
+export * as log from "https://deno.land/std@0.107.0/log/mod.ts"
