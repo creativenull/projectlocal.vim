a51afc4aabae1a39db05a62aad6f503dcde3b5d7
Arnold Chand <creativenull@outlook.com>
2021-11-22 16:44:13 -0400

    fix(core/hasher): decode hashed content as hex string

--- denops/projectlocal/hasher.ts
+++ denops/projectlocal/hasher.ts
@@ -1,16 +1,14 @@
 import { crypto } from "./deps/std.ts";
 
 function encodeString(contents: string): Uint8Array {
-  return (new TextEncoder()).encode(contents)
-}
-
-function decodeBuffer(contents: Uint8Array | ArrayBuffer): string {
-  return (new TextDecoder('utf-8')).decode(contents);
+  return (new TextEncoder()).encode(contents);
 }
 
 export function hashFileContents(contents: string): string {
-  const result = crypto.subtle.digestSync('SHA-256', encodeString(contents));
-  return decodeBuffer(result);
-  /* const hasher = hash.createHash("sha256");
-  return hasher.update(contents).toString(); */
+  const encodedContents = encodeString(contents);
+  const hashedResult = crypto.subtle.digestSync("SHA-256", encodeString);
+  const decodedResult = Array.from(new Uint8Array(hashedResult));
+  const hexStringResult = decodedResult
+    .map((b) => b.toString(16).padStart(2, "0")).join("");
+  return hexStringResult;
 }
