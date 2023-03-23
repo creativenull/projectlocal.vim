91d94b9d3b503d032231837d8fc5065cb9a147e8
Arnold Chand <creativenull@outlook.com>
2023-03-23 11:58:20 -0400

    fix: class to func, default msg

--- denops/projectlocal/message.ts
+++ denops/projectlocal/message.ts
@@ -3,28 +3,42 @@ import { Denops, fn } from "./deps/denops_std.ts";
 export const pluginName = "[projectlocal]";
 
 /**
- * Perpare message with plugin name prepended
+ * Message template to use for :echo messages.
+ *
  * @param {string} msg
+ * @returns {string}
  */
-export function info(msg: string) {
+export function info(msg: string): string {
   return `${pluginName} ${msg}`;
 }
 
-export async function confirmFirstTime(
-  denops: Denops,
-  msg: string,
-) {
+/**
+ * Prompt user for permission on first time session.
+ *
+ * @async
+ * @param {Denops} denops
+ * @returns {Promise<1 | 2 | 3 | 4>}
+ */
+export async function confirmFirstTime(denops: Denops): Promise<1 | 2 | 3 | 4> {
+  const msg = "New config file found, include it?";
   return await fn.confirm(
     denops,
     `${pluginName} ${msg}`,
     "&Yes (Always)\n&No (Do not prompt again)\n&Open Config\n&Cancel",
     4,
-  );
+  ) as 1 | 2 | 3 | 4;
 }
 
-export async function confirmOnChange(
-  denops: Denops,
-  msg: string,
-) {
-  return await fn.confirm(denops, `${pluginName} ${msg}`, "&Yes\n&No", 2);
+/**
+ * Prompt user for permission on config file changes.
+ *
+ * @async
+ * @param {Denops} denops
+ * @returns {Promise<1 | 2>}
+ */
+export async function confirmOnChange(denops: Denops): Promise<1 | 2> {
+  const msg = "Config file changed, re-include changes?";
+  return await fn.confirm(denops, `${pluginName} ${msg}`, "&Yes\n&No", 2) as
+    | 1
+    | 2;
 }
