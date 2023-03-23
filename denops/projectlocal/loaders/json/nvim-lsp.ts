88b270dd606b27c84fb92de03bd9ca76779eb5ff
Arnold Chand <creativenull@outlook.com>
2023-03-23 12:03:26 -0400

    feat: json plugins to handle diff config

---
+++ denops/projectlocal/loaders/json/nvim-lsp.ts
@@ -0,0 +1,36 @@
+import type { Denops } from "../../deps/denops_std.ts";
+import { helpers } from "../../deps/denops_std.ts";
+import type { UserConfig } from "../../config.ts";
+
+/**
+ * Plugin to setup nvim-lsp servers.
+ *
+ * @async
+ * @param {Denops} denops
+ * @param {UserConfig} config
+ * @param {string[] | NvimLspConfig[]} nvimLspContext
+ * @returns {Promise<void>}
+ */
+export async function handle(
+  denops: Denops,
+  config: UserConfig,
+  nvimLspContext: string[] | NvimLspConfig[],
+): Promise<void> {
+  const serializeConfig = JSON.stringify(config);
+  const serializeContext = JSON.stringify(nvimLspContext);
+  await helpers.execute(
+    denops,
+    `lua require("projectlocal.lsp").register_lspservers([=[${serializeContext}]=], [=[${serializeConfig}]=])`,
+  );
+}
+
+export type NvimLspConfig = {
+  name: string;
+  config?: {
+    init_options?: Record<string, unknown>;
+    settings?: Record<string, unknown>;
+    root_dir?: Record<string, unknown>;
+    filetypes?: string[];
+    single_file_support?: boolean;
+  };
+};
