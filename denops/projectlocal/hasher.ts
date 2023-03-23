7079a96ce1076db1ad7ad9a6c85cb125c8254341
Arnold Chand <creativenull@outlook.com>
2023-03-23 11:57:20 -0400

    fix: rename, add docs

--- denops/projectlocal/hasher.ts
+++ denops/projectlocal/hasher.ts
@@ -1,7 +1,15 @@
 import { createHash } from "./deps/std.ts";
 
-export async function hashFileContents(contents: string): Promise<string> {
+/**
+ * Generate a hash of the string contents.
+ *
+ * @async
+ * @param {string} contents
+ * @returns {Promise<string>}
+ */
+export async function getHashFileContents(contents: string): Promise<string> {
   const result = await createHash("SHA-256", contents);
   const arrayResult = Array.from(new Uint8Array(result));
+
   return arrayResult.map((b) => b.toString(16).padStart(2, "0")).join("");
 }
