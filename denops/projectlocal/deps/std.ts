e0d95d14a52e1220e739553ae8f96f1edcc73d78
Arnold Chand <creativenull@outlook.com>
2024-03-05 22:13:57 -0400

    fix: use web crypto digest hasher

--- denops/projectlocal/deps/std.ts
+++ denops/projectlocal/deps/std.ts
@@ -1,2 +1,1 @@
-export { createHash } from "https://deno.land/std@0.164.0/crypto/_util.ts";
 export * as fs from "https://deno.land/std@0.164.0/fs/mod.ts";
--- denops/projectlocal/hasher.ts
+++ denops/projectlocal/hasher.ts
@@ -1,15 +1,12 @@
-import { createHash } from "./deps/std.ts";
+import { crypto } from "jsr:@std/crypto@0.218.2";
 
 /**
  * Generate a hash of the string contents.
- *
- * @async
- * @param {string} contents
- * @returns {Promise<string>}
  */
 export async function getHashFileContents(contents: string): Promise<string> {
-  const result = await createHash("SHA-256", contents);
-  const arrayResult = Array.from(new Uint8Array(result));
-
-  return arrayResult.map((b) => b.toString(16).padStart(2, "0")).join("");
+  const encodedHash = await crypto.subtle.digest(
+    "SHA-256",
+    (new TextEncoder()).encode(contents),
+  );
+  return (new TextDecoder()).decode(encodedHash);
 }
